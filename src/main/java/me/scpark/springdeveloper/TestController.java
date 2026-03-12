8091e64d7deebfce54342600f2944abd8bba2146
박지민 <snam113@g.yju.ac.kr>
2026-03-12 12:14:29 +0900

    init: Spring Boot project setup

--- src/main/java/me/scpark/springdeveloper/TestController.java
+++ src/main/java/me/scpark/springdeveloper/TestController.java
@@ -3,15 +3,18 @@ package me.scpark.springdeveloper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
+// 모든 메소드가 데이터를 리턴한다
+//@Controller  기본적으로 메소드 리턴하는 것이  HTML 파일
 public class TestController {
     @Autowired
-    TestService testService;
+    private TestService testService;
+
     @GetMapping("/test")
     public List<Member> getAllMembers() {
-        List<Member> members = testService.getAllMembers();
-        return members;
+        return testService.getAllMembers();
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO member (id,name) VALUES (1, 'name')
+INSERT INTO member (id,name) VALUES (2, 'name2')
+INSERT INTO member (id,name) VALUES (3, 'name3')
