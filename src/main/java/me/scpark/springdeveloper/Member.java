b6cda51c8422b40b2e2098b8adb00d4d721f6020
박지민 <snam113@g.yju.ac.kr>
2026-04-02 12:23:03 +0900

    init: Spring Boot project setup

--- src/main/java/me/scpark/springdeveloper/Member.java
+++ src/main/java/me/scpark/springdeveloper/Member.java
@@ -2,23 +2,24 @@ package me.scpark.springdeveloper;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
-@Getter
 @Entity
+@Getter
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", updatable = false)
     private Long id;
     @Column(name = "name", nullable = false)
     private String name;
 
-    public Member(String name) {
-        this.name=name;
+    public void changeName(String name) {
+        this.name = name;
     }
+
+    public Member(String name) {
+        this.name = name;
+        }
 }
